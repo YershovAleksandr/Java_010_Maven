1bfcd57b8b06f2f2826933a594932e5a40b0b7d4
hp <namstudionsk@gmail.com>
2018-02-17 22:22:19 +0700

    Fix lv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 public class Main {
-    public static void Main(String[] args){
+    public static void main(String[] args){
         //
         System.out.println("Hello fucking world");
+
+        Logger log = LoggerFactory.getLogger(Main.class);
+
+        log.info("Wtf");
+
     }
 }
