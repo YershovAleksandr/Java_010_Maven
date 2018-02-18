f85809fdc939c7640c34cb454f2e7959fa6b31d7
hp <namstudionsk@gmail.com>
2018-02-19 03:58:40 +0700

    Fix lv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,13 @@
 
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.NiftyInputConsumer;
+import de.lessvoid.nifty.nulldevice.NullSoundDevice;
+import de.lessvoid.nifty.slick2d.input.SlickInputSystem;
+import de.lessvoid.nifty.slick2d.render.SlickRenderDevice;
+import de.lessvoid.nifty.spi.time.TimeProvider;
+import de.lessvoid.nifty.tools.resourceloader.NiftyResourceLoader;
+import org.newdawn.slick.Game;
+import org.newdawn.slick.GameContainer;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
@@ -10,6 +19,18 @@ public class Main {
         Logger log = LoggerFactory.getLogger(Main.class);
 
         log.info("Wtf");
+
+        //SlickInputSystem is = new SlickInputSystem();
+
+
+
+
+        //Nifty nifty = new Nifty(new SlickRenderDevice(new GameContainer(new Game())), new NullSoundDevice(), new SlickInputSystem(), new TimeProvider());
+        //Nifty nifty = new Nifty(new LwjglRenderDevice(new GameContainer(new Game())), new NullSoundDevice(), new SlickInputSystem(), new TimeProvider());
+
+       // String vr = nifty.getVersion();
+
+        //log.info("Vr = " + vr);
 
     }
 }
